94952ebd51297826f6643feb9f3b0480787ca349
Ujwal Kumar Yadav <ujwalofficial99@gmail.com>
2022-11-15 19:50:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ app.use("/api/messages", require("./routes/messagesRoute"));
 
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV == "production") {
-  const __dirname1 = path.resolve();
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+// if (process.env.NODE_ENV == "production") {
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+);
+// } else {
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+// }
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
